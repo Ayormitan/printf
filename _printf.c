55b20dc19db1f9cca8d0248892869b430b68da1f
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-16 16:09:40 -0700

    Printf function

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
  *
  *
  */
-void _printbuffer(char buffer, int *buffindex)
+void _printbuffer(char buffer[], int *buffindex)
 {
 	if (*buffindex > 0)
 	{
@@ -25,12 +25,13 @@ void _printbuffer(char buffer, int *buffindex)
 int _printf(const char *format, ...)
 {
 	int printchar = 0;
-	int i;
+	int i, width = 0;
+	int printed = 0;
 	int buffindex = 0;
 	char buffer[BUFFER_SIZE];
 	va_list lists;
 
-	if (format == NULL)
+	if (format == NULL || format[0] == '\0')
 	{
 		return (-1);/** the function exits if theirs is nothing in the format string*/
 	}
@@ -41,9 +42,20 @@ int _printf(const char *format, ...)
 		{
 			buffer[buffindex++] = format[i];
 			if (buffindex == BUFFER_SIZE)
+			{
 				_printbuffer(buffer, &buffindex);/** During string parsing if BUFFER_SIZE ia full*/
 				/** we use rite already implemented as _printbuffer to print to stdout*/
 				printchar++;
+			}
+		}
+			else
+		{
+				_printbuffer(buffer, &buffindex);
+				++i;
+				printed = _handleprints(format, &i, lists, buffer, width);
+				if (printed == -1)
+					return (-1);
+				printchar += printed;
 		}
 	}
 	_printbuffer(buffer, &buffindex);
