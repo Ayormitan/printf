59ad8a4934c693435614ccdf96bb0c2f0a3b4c74
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-16 16:06:44 -0700

    Header file

--- main.h
+++ main.h
@@ -6,13 +6,18 @@
 
 #define BUFFER_SIZE 1024
 
-void _printbuffer(char buffer, int *buffindex);
+/** flags */
+#define F_ZERO 4
+#define F_MINUS 1
+#define UNUSED(x) (void)(x)
+
+void _printbuffer(char buffer[], int *buffindex);
 int _printf(const char *format, ...);
 
 /** prototype handling specifiers **/
 int _printschar(va_list args, char buffer[]);
 int _printstring(va_list args, char buffer[]);
-int _printspercent(va_list arg, char buffer[]);
+int _printspercent(va_list args, char buffer[]);
 
 /**
  *
@@ -28,6 +33,8 @@ typedef struct formt formt_t;
 /**
  *
  */
-int _handleprints(const char *formt, int *indx, va_list lists, char buffer[]);
+int _handleprints(const char *formt, int *indx, va_list lists, char buffer[], int width);
 
+/** Write char and string to buffer */
+int write_character_to_buffer(char c, char buffer[], int width, int flags);
 #endif
