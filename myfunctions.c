323401cac8fba17616843c0ef5921d55973833a5
Brightbin <brightndubisi23@gmail.com>
2023-10-17 14:23:51 -0700

    documentation

--- myfunctions.c
+++ myfunctions.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * _writenumber - writes number to a char buffer
+ * @is_neg: indicated if a number is negative
+ * @indx: represents current in buffer
+ * @buffer: char array to store numbers
+ * Return: result of write function
  */
 int _writenumber(int is_neg, int indx, char buffer[])
 {
@@ -13,9 +18,15 @@ int _writenumber(int is_neg, int indx, char buffer[])
 }
 
 /**
+ * _writenum - writes num to char buffer
+ * @indx: current index in buffer
+ * @buffer: array to store number
+ * @paddlin: unsed
+ *
+ * @extra_char: extra character written before number
+ * Return: result of write function
  */
-int _writenum(int indx, char buffer[],
-       	char paddlin, char extra_char)
+int _writenum(int indx, char buffer[], char paddlin, char extra_char)
 {
 	int len = BUFFER_SIZE - indx - 1;
 
