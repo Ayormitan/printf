c32bd6b588f0f77e06061c6105cc73c98b249bc7
Brightbin <brightndubisi23@gmail.com>
2023-10-18 04:34:06 -0700

    convert size

--- _utilities.c
+++ _utilities.c
@@ -17,3 +17,23 @@ long int convert_unsignedint_to_bin(long int number, int size)
 	}
 	return ((unsigned int)number);
 }
+
+/**
+ * convsize_num - converts an int to number
+ * @num: long int representing int
+ * @size: int representing size
+ * Return: converted unsigned int
+ */
+long int convsize_num(long int num, int size)
+{
+	if (size == LONG_H)
+	{
+		return (num);
+	}
+	else if (size == SHORT_H)
+	{
+		return ((short)num);
+	}
+
+	return ((int)num)
+}
